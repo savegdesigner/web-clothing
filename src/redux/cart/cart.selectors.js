38a97e6b8b0e389aaf1e0b6613234e689a311b54
savegnago <savegdesigner@gmail.com>
2020-08-20 21:13:11 -0300

    redux persist :rocket:

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -15,7 +15,8 @@ export const selectCartHidden = createSelector(selectCart, (cart) => cart.hidden
 
 export const selectCartTotal = createSelector(selectCartItems, (cartItems) =>
   cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.price,
+    (accumulatedQuantity, cartItem) =>
+      accumulatedQuantity + cartItem.price * cartItem.quantity,
     0
   )
 );
